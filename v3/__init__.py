3ab080cd42ddda0a2f255d4d9cfc7223cc5d74e1
ElicoJr <elicogjunior@gmail.com>
2025-11-21 23:45:13 -0400

    Atualiza o arquivo __init__.py para incluir novas funções de otimização e carregamento de dados.

--- v3/__init__.py
+++ v3/__init__.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # v3/__init__.py
 
 from .optimization import MetaHeuristicaV3
@@ -8,10 +7,4 @@ __all__ = [
     "MetaHeuristicaV3",
     "prepare_equipes_v3",
     "prepare_pendencias_v3",
-=======
-# v3 package for enhanced routing simulation
-
-__all__ = [
-    "data_loader_v3",
->>>>>>> adf5a9eb0e369fdaac2a596ee5a134a92492311c
 ]
